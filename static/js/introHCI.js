675c7a8f392dd67d6149e89655926c1fe44a7257
Derrick Liu <derrick56007@hotmail.com>
2019-01-24 10:11:18 -0800

    work

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,18 +1,39 @@
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
-})
+$(document).ready(function () {
+    initializePage();
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+    $("#testjs").click(function (e) {
+        $('.jumbotron h1').text("Javascript has taken control");
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+        $('#testjs').text('What is Love?');
+
+        $('.jumbotron p').toggleClass('active');
+    });
+
+    $(".project").click(projectClick)
+
+    // Add any additional listeners here
+    // example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+    e.preventDefault();
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+        description.fadeOut(400, function (e) {
+            description.remove();
+        });
+        // description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
 }
